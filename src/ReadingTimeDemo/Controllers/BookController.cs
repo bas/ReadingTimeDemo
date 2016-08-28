bdbf23c61a0d03214d961976e5b8c9b6179fcd32
Bas Peters <bas@github.com>
2016-08-28 20:49:55 +0200

    added book controller, model, view and service

---
+++ src/ReadingTimeDemo/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReadingTimeDemo.Services;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReadingTimeDemo.Controllers
+{
+    public class BookController : Controller
+    {
+        private BookService bookService = new BookService();
+
+        public IActionResult Index()
+        {
+            return View(bookService.Books);
+        }
+    }
+}
---
+++ src/ReadingTimeDemo/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReadingTimeDemo.Models
+{
+    public class Book
+    {
+        [Required]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Cover { get; set; }
+
+        public Book()
+        {
+
+        }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+        public Book(string title, string author, string cover)
+        {
+            Title = title;
+            Author = author;
+            Cover = cover;
+        }
+    }
+
+}
