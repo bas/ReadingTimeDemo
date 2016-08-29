5c3f2cf29343f24a8ec83f994a9f033325e76a07
Bas Peters <bas@github.com>
2016-08-29 17:24:22 +0200

    changed landing page to book

--- src/ReadingTimeDemo/Startup.cs
+++ src/ReadingTimeDemo/Startup.cs
@@ -61,7 +61,7 @@ namespace ReadingTimeDemo
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Book}/{action=Index}/{id?}");
             });
         }
     }
