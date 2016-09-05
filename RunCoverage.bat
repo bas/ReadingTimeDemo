dea92f3844849915dd5e20fe3f3fad6f6bc0a192
Bas Peters <bas@github.com>
2016-09-05 22:26:27 +0200

    Added coveralls

--- RunCoverage.bat
+++ RunCoverage.bat
@@ -4,10 +4,12 @@ SET filter="+[ReadingTimeDemo]* -[ReadingTimeDemo]*Startup -[ReadingTimeDemo]*Pr
 :: Update this when OpenCover or ReportGenerator are updated
 SET opencoverversion=4.6.519
 SET reportgeneratorversion=2.4.5
+SET coverallsversion=1.3.4
 
 SET packages=C:\Users\appveyor\.nuget\packages
 SET opencover=%packages%\OpenCover\%opencoverversion%\tools\OpenCover.Console.exe
 SET reportgenerator=%packages%\ReportGenerator\%reportgeneratorversion%\tools\ReportGenerator.exe
+SET coveralls=%packages%\coveralls.io\%coverallsversion%\tools\coveralls.net.exe
 
 SET targetdir=%~dp0test\ReadingTimeDemo.UnitTests\bin\Debug\netcoreapp1.0
 SET coveragedir=coverage
@@ -19,5 +21,7 @@ SET coveragefile=coverage.xml
 SET corerunargs=test "%~dp0test\ReadingTimeDemo.UnitTests\project.json" -xml %resultsfile%
 
 %opencover% -oldStyle -filter:%filter% -register:user -targetdir:%targetdir% -target:dotnet.exe -output:%coveragefile% -targetargs:"%corerunargs%"
+
+%coveralls% --opencover %coveragefile% --full-sources
 
 %reportgenerator% -targetdir:%coveragedir% -reporttypes:Html -reports:%coveragefile% -verbosity:Error
