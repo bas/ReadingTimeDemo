a565c0d68563978957f3b3b1b498c358605fbe27
Bas Peters <bas@github.com>
2016-09-20 10:43:29 +0200

    Added initial coverage script in powrshell

--- RunCoverage.cmd
+++ RunCoverage.cmd
@@ -1,11 +1,12 @@
 @echo off
+SET requiredcoverage=90
 SET filter="+[ReadingTimeDemo]* -[ReadingTimeDemo]*Startup -[ReadingTimeDemo]*Program"
 
 SET opencoverversion=4.6.519
 SET reportgeneratorversion=2.4.5
 SET coverallsversion=1.3.4
 
-SET packages=C:\Users\appveyor\.nuget\packages
+SET packages=C:\Users\bas\.nuget\packages
 SET opencover=%packages%\OpenCover\%opencoverversion%\tools\OpenCover.Console.exe
 SET reportgenerator=%packages%\ReportGenerator\%reportgeneratorversion%\tools\ReportGenerator.exe
 SET coveralls=%packages%\coveralls.io\%coverallsversion%\tools\coveralls.net.exe
@@ -19,6 +20,17 @@ SET corerunargs=test "%~dp0test\ReadingTimeDemo.UnitTests\project.json"
 
 %opencover% -oldStyle -filter:%filter% -register:user -targetdir:%targetdir% -target:dotnet.exe -output:%coveragefile% -targetargs:"%corerunargs%"
 
-%coveralls% --opencover %coveragefile% --full-sources
+::%coveralls% --opencover %coveragefile% --full-sources
 
-::%reportgenerator% -targetdir:%coveragedir% -reporttypes:Html -reports:%coveragefile% -verbosity:Error
+%reportgenerator% -targetdir:%coveragedir% -reporttypes:Html -reports:%coveragefile% -verbosity:Error
+
+set "coverage="
+for /f "tokens=6 delims=<>" %%a in (
+  'find /i "Line coverage:" ^< "%~dp0%coveragedir%\index.htm"'
+) do set "coverage=%%a"
+
+if %coverage% geq %requiredcoverage% (
+  echo Pass: the coverage percentage is: %coverage%
+) else (
+  echo Fail: the coverage is %coverage%
+)
---
+++ runcoverage.ps1
@@ -0,0 +1,62 @@
+$RequiredCoverage=90
+$User="bas"
+$Filter="+[ReadingTimeDemo]* -[ReadingTimeDemo]*Startup -[ReadingTimeDemo]*Program"
+
+$OpenCoverVersion="4.6.519"
+$ReportGeneratorVersion="2.4.5"
+
+$Packages="C:\Users\$User\.nuget\packages"
+$OpenCover="$Packages\OpenCover\$OpenCoverVersion\tools\OpenCover.Console.exe"
+$ReportGenerator="$Packages\ReportGenerator\$ReportGeneratorVersion\tools\ReportGenerator.exe"
+
+$WorkingDir= Get-Location
+$TargetDir="$WorkingDir\test\ReadingTimeDemo.UnitTests\bin\Debug\netcoreapp1.0"
+$CoverageDir="coverage"
+
+$CoverageFile="coverage.xml"
+
+$AllArgs = @("-oldStyle","-filter:$Filter", "-register:user","-targetdir:$TargetDir", '-target:dotnet.exe', "-output:$CoverageFile", "-targetargs:test $WorkingDir\test\ReadingTimeDemo.UnitTests")
+
+& $OpenCover $AllArgs
+
+& $ReportGenerator -targetdir:$CoverageDir -reporttypes:Html -reports:$CoverageFile -verbosity:Error
+
+$PercentageValue
+
+type "$WorkingDir\$CoverageDir\index.htm" | where { $_ -match "Line coverage:</th><td>(?<percentage>[A-Z0-9.%]+)</td></tr>" } | foreach { $PercentageValue = $matches['percentage'] }
+
+Write-Host "Coverage percentage: $PercentageValue"
+
+$Status = "failure"
+$Message = "Coverage check failed to reach 90%" 
+
+if ($PercentageValue -ge 90) {
+	$Status = "success"
+	$Message = "Coverage check is 90% or higher"
+}
+
+$RepoSlug = $env:APPVEYOR_REPO_NAME
+$Commit = $env:APPVEYOR_REPO_COMMIT
+$Token = $env:GITHUB_TOKEN
+
+$Data = @{
+	state = $Status;
+	target_url = "https://appveyor.com/";
+	description = $Message;
+	context = "coverage-status-check/opencover";
+}
+
+$ReleaseParams = @{
+   Uri = "https://octodemo.com/api/v3/repos/$RepoSlug/statuses/$Commit";
+   Method = 'POST';
+   Headers = @{
+     Authorization = 'Basic ' + [Convert]::ToBase64String(
+     [Text.Encoding]::ASCII.GetBytes($Token + ":x-oauth-basic"));
+   }
+   ContentType = 'application/json';
+   Body = (ConvertTo-Json $Data -Compress)
+ }
+
+ $Result = Invoke-RestMethod @ReleaseParams
+
+ Write-Host $Result
