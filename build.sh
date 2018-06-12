2050527ce16c474adaa088fec2a609d6fad14c50
Bas Peters <bas@github.com>
2018-06-12 16:31:26 +0200

    Remove docker compose files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-dotnet restore ./ReadingTimeDemo.sln
-dotnet test test/ReadingTimeDemo.UnitTests/ReadingTimeDemo.UnitTests.csproj
-
-rm -rf ./obj/Docker/publish
-dotnet publish ./ReadingTimeDemo.sln -c Release -o ./obj/Docker/publish
