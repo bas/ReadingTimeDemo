171af4029d689edcc025c44d64d59ac35e6643a1
Bas Peters <bas@github.com>
2018-07-20 12:51:21 +0200

    Fix areguments issue for Moore book object

--- src/ReadingTimeDemo/Services/BookService.cs
+++ src/ReadingTimeDemo/Services/BookService.cs
@@ -9,7 +9,7 @@ namespace ReadingTimeDemo.Services
         {
             new Book("Scrum: The Art of Doing Twice the Work in Half the Time", "Jeff Sutherland", "scrum.jpg"),
             new Book("The Lean Startup: How Constant Innovation Creates Radically Successful Businesses","Eric Ries", "lean.jpg"),
-            new Book("Crossing the Chasm", "Geoffrey A. Moore", "chasm.jpg", 5),
+            new Book("Crossing the Chasm", "Geoffrey A. Moore", "chasm.jpg"),
             //new Book("The Pragmatic Programmer: From Journeyman to Master", "David Thomas", "pragmatic.jpg"),
             new Book("Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability"," Steve Krug", "think.jpg")
         };
