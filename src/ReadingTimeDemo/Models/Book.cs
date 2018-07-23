2a009b0ae3ab0b533230b3a7f503b6e92cf214aa
Bas Peters <bas@github.com>
2018-07-23 15:51:13 +0200

    Added rating model, view and service

--- src/ReadingTimeDemo/Models/Book.cs
+++ src/ReadingTimeDemo/Models/Book.cs
@@ -12,6 +12,8 @@ namespace ReadingTimeDemo.Models
         public string Author { get; set; }
         public string Cover { get; set; }
 
+        private long rating;
+
         public Book()
         {
 
@@ -29,5 +31,32 @@ namespace ReadingTimeDemo.Models
             Cover = cover;
         }
 
+        public Book(string title, string author, string cover, int rating)
+        {
+            Title = title;
+            Author = author;
+            Cover = cover;
+            Rating = rating;
+        }
+
+        public int Rating
+        {
+            get { return rating; }
+            set
+            {
+                ValidateRating(value);
+                rating = value;
+            }
+        }
+
+        public static double ValidateRating(int input)
+        {
+            if (input < 0 || input > 5)
+            {
+                throw new System.ArgumentOutOfRangeException("Rating must be a value between 0 and 5");
+            }
+            return input;
+        }
     }
+
 }
