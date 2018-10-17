57d0e6c744d8576084a39d0ebe25cf92077c3dd4
Baloro Craftwork <bas+baloro@github.com>
2018-10-17 10:43:07 +0200

    Made list private

--- src/ReadingTimeDemo/Services/BookService.cs
+++ src/ReadingTimeDemo/Services/BookService.cs
@@ -5,7 +5,7 @@ namespace ReadingTimeDemo.Services
 {
     public class BookService
     {
-        public List<Book> Books = new List<Book>
+        private List<Book> books = new List<Book>
         {
             new Book("Scrum: The Art of Doing Twice the Work in Half the Time", "Jeff Sutherland", "scrum.jpg", 3),
             new Book("The Lean Startup: How Constant Innovation Creates Radically Successful Businesses","Eric Ries", "lean.jpg", 4),
@@ -13,5 +13,7 @@ namespace ReadingTimeDemo.Services
             //new Book("The Pragmatic Programmer: From Journeyman to Master", "David Thomas", "pragmatic.jpg", 2),
             new Book("Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability"," Steve Krug", "think.jpg", 4)
         };
+
+        public List<Book> Books() => books;
     }
 }
