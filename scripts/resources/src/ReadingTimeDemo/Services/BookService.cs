2bfd2afa7e51df6ef345656dce48599d630305e4
Baloro Craftwork <bas+baloro@github.com>
2018-10-17 20:54:01 +0200

    Break build in scripts

--- scripts/resources/src/ReadingTimeDemo/Services/BookService.cs
+++ scripts/resources/src/ReadingTimeDemo/Services/BookService.cs
@@ -10,7 +10,7 @@ namespace ReadingTimeDemo.Services
             new Book("Scrum: The Art of Doing Twice the Work in Half the Time", "Jeff Sutherland", "scrum.jpg", 3),
             new Book("The Lean Startup: How Constant Innovation Creates Radically Successful Businesses","Eric Ries", "lean.jpg", 4),
             new Book("Crossing the Chasm", "Geoffrey A. Moore", "chasm.jpg", 5),
-            //new Book("The Pragmatic Programmer: From Journeyman to Master", "David Thomas", "pragmatic.jpg", 2),
+            new Book("The Pragmatic Programmer: From Journeyman to Master", "David Thomas", "pragmatic.jpg", 2),
             new Book("Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability"," Steve Krug", "think.jpg", 4)
         };
 
