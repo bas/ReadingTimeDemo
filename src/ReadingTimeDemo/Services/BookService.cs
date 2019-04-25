3a138b9f78c145571aa076e9bd08b741dae97b73
Bas Peters <bas@github.com>
2019-04-25 18:48:42 +0200

    Update BookService.cs

--- src/ReadingTimeDemo/Services/BookService.cs
+++ src/ReadingTimeDemo/Services/BookService.cs
@@ -10,7 +10,7 @@ namespace ReadingTimeDemo.Services
             new Book("Scrum: The Art of Doing Twice the Work in Half the Time", "Jeff Sutherland", "scrum.jpg"),
             //new Book("The Lean Startup: How Constant Innovation Creates Radically Successful Businesses","Eric Ries", "lean.jpg"),
             new Book("Crossing the Chasm", "Geoffrey A. Moore", "chasm.jpg"),
-            new Book("The Pragmatic Programmer: From Journeyman to Master", "David Thomas", "pragmatic.jpg"),
+            new Book("The Extreme Pragmatic Programmer: From Journeyman to Master", "David Thomas", "pragmatic.jpg"),
             new Book("Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability"," Steve Krug", "think.jpg")
         };
 
