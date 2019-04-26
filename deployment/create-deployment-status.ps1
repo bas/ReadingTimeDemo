03be7b11f75d41af15716dba9c1b8513ff97e689
Bas Peters <bas@github.com>
2019-04-26 12:13:11 +0200

    Update create-deployment-status.ps1

--- deployment/create-deployment-status.ps1
+++ deployment/create-deployment-status.ps1
@@ -1,6 +1,6 @@
 Write-Host "Reading:" $env:DeployId
 
-$url = "https://api.octodemo.com/v3/repos/office-tools/ReadingTimeDemo/deployments/$env:DeployId/statuses"
+$url = "https://octodemo.com/api/v3/repos/office-tools/ReadingTimeDemo/deployments/$env:DeployId/statuses"
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "Token $env:MAPPED_GITHUB_TOKEN")
